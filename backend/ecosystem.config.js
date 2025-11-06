70f592345850819f2fcaa16ad32534e7b23e1cdd
Miraz Rafi <mirazrafi31@gmail.com>
2025-11-06 19:55:57 +0600

    Add PM2 manager

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [{
+    name: 'codechat-api',
+    script: './src/server.js',
+    cwd: '/home/miraz-rafi/codeChat/backend',
+    instances: 1,
+    exec_mode: 'fork',
+    watch: false,
+    max_memory_restart: '500M',
+    env: {
+      NODE_ENV: 'development',
+    },
+    env_production: {
+      NODE_ENV: 'production',
+    },
+    error_file: './logs/err.log',
+    out_file: './logs/out.log',
+    log_file: './logs/combined.log',
+    time: true,
+    autorestart: true,
+    max_restarts: 10,
+    min_uptime: '10s',
+    restart_delay: 4000,
+  }]
+};
